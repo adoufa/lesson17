fa607fc3b1c861472422be864550d4f2ce3406ed
adoufa <arinadolgaya@icloud.com>
2023-01-12 19:54:44 +0300

    2 commit

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
@@ -10,3 +11,23 @@ def index(request):
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = "Форма не заполнена"
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
+
+
